a587a7eba3427e9ea50cefa5911a6f1e85dce1e1
hasibalasad <hasibaveiro007@gmail.com>
2026-02-24 09:04:37 +0600

    improve error handling, and enhance contact management features

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -2,22 +2,29 @@ import { redirect } from "react-router-dom";
 import { createContact, deleteContact, updateContact } from "../contacts";
 
 export async function contactAction() {
-    const contact = await createContact();
-    return { contact };
+  const contact = await createContact();
+  return { contact };
 }
 
 export async function editContactAction({ request, params }) {
-    const formData = await request.formData();
-    const updates = Object.fromEntries(formData.entries());
-    await updateContact(params.contactId, updates);
-    return redirect(`/contacts/${params.contactId}`);
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData.entries());
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
 }
 
 export async function destroyContactAction({ params }) {
-    try {
-        await deleteContact(params.contactId);
-    } catch (error) {
-        throw new Error("Failed to delete", error);
-    }
-    return redirect("/");
+  try {
+    await deleteContact(params.contactId);
+  } catch (error) {
+    throw new Error("Failed to delete", error);
+  }
+  return redirect("/");
+}
+
+export async function updateFavoriteContactAction({ request, params }) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,100 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { contactAction, destroyContactAction, editContactAction } from './actions/contactAction'
-import ErrorPage from './ErrorPage'
-import Index from './Index'
-import './index.css'
-import { getContactLoader, getContactsLoader } from './loaders/contactsLoader'
-import Root from './Root'
-import Contact from './routes/Contact'
-import EditContact from './routes/Edit'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  contactAction,
+  destroyContactAction,
+  editContactAction,
+  updateFavoriteContactAction,
+} from "./actions/contactAction";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        loader: getContactsLoader,
-        action: contactAction,
-        children: [
-            {
-                index: true,
-                element: <Index />
-            },
-            {
-                path: "contacts/:contactId",
-                element: <Contact />,
-                loader: getContactLoader,
-            },
-            {
-                path: "contacts/:contactId/edit",
-                element: <EditContact />,
-                loader: getContactLoader,
-                action: editContactAction,
-            },
-            {
-                path: "contacts/:contactId/destroy",
-                action: destroyContactAction,
-                errorElement: <ErrorPage />
-            },
-        ]
-    }
-])
+import ErrorPage from "./ErrorPage";
+import Index from "./Index";
+import "./index.css";
+import { getContactLoader, getContactsLoader } from "./loaders/contactsLoader";
+import Root from "./Root";
+import Contact from "./routes/Contact";
+import EditContact from "./routes/Edit";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>,
-)
+//? 1st way to create router
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     errorElement: <ErrorPage />,
+//     loader: getContactsLoader,
+//     action: contactAction,
+//     children: [
+//       {
+//         errorElement: <ErrorPage />,
+//         children: [
+//           {
+//             index: true,
+//             element: <Index />,
+//           },
+//           {
+//             path: "contacts/:contactId",
+//             element: <Contact />,
+//             loader: getContactLoader,
+//             action: updateFavoriteContactAction,
+//           },
+//           {
+//             path: "contacts/:contactId/edit",
+//             element: <EditContact />,
+//             loader: getContactLoader,
+//             action: editContactAction,
+//           },
+//           {
+//             path: "contacts/:contactId/destroy",
+//             action: destroyContactAction,
+//             errorElement: <ErrorPage />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
+
+//? 2nd way to create router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={getContactsLoader}
+      action={contactAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index={true} element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={getContactLoader}
+          action={updateFavoriteContactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={getContactLoader}
+          action={editContactAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyContactAction}
+        />
+      </Route>
+    </Route>,
+  ),
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
